7d6c4c899bfc43c720d3a1d41d8be4a546d7bc3c
Rommel G. H <rommelgh@gmail.com>
2019-06-05 21:56:16 -0500

    Including if implementation.

--- logo_compiler.py
+++ logo_compiler.py
@@ -2,30 +2,38 @@
 
 import sys
 import re
+import inspect
 
 tokens = {
-    "number":       (0,r'\d+'),
-    "rint":         (1,r'\$int'),
-    "begin":        (2,r'\$begin'),
-    "end":          (3,r'\$end'),
-    "move":         (4,r'\$move'),
-    "turn":         (5,r'\$turn'),
-    "define":       (6,r'\$define'),
-    "arg_right":    (7,r'right'),
-    "arg_forward":  (8,r'forward'),
-    "identifier":   (9,r'[a-zA-Z][a-zA-Z0-9_]*'),
-    "oparenthesis": (10,r'\('),
-    "cparenthesis": (11,r'\)'),
-    "comma":        (12,r','),
-    "equal":        (13,r':='),
-    "plus":         (14,r'\+'),
-    "minus":        (15,r'\-'),
-    "comment":      (16,r'//.*'),
-    "newline":      (17,r'\n'),
-    "whitespace":   (18,r' '),
-    "rstring":      (19,r'\$string'),
-    "print":        (20,r'\$print'),
-    "string":       (21,r'\".*\"')
+    "number":       (0,r'\d+',""),
+    "rint":         (1,r'\$int',"make"),
+    "begin":        (2,r'\$begin',""),
+    "end":          (3,r'\$end',""),
+    "move":         (4,r'\$move',""),
+    "turn":         (5,r'\$turn',""),
+    "define":       (6,r'\$define',""),
+    "if":           (7,r'\$if',"if"),
+    "arg_right":    (8,r'right',"right"),
+    "arg_left":     (9,r'left',"left"),
+    "arg_forward":  (10,r'forward',"forward"),
+    "arg_backward": (11,r'backward',"backward"),
+    "identifier":   (12,r'[a-zA-Z][a-zA-Z0-9_]*',""),
+    "oparenthesis": (13,r'\(',""),
+    "cparenthesis": (14,r'\)',""),
+    "comma":        (15,r',',""),
+    "equal":        (16,r':=',""),
+    "plus":         (17,r'\+',""),
+    "minus":        (19,r'\-',""),
+    "divide":       (20,r'\/',""),
+    "multiply":     (21,r'\*',""),
+    ">":            (22,r'\>',""),
+    "<":            (23,r'\<',""),
+    "comment":      (24,r'//.*',""),
+    "newline":      (25,r'\n',""),
+    "whitespace":   (26,r' ',""),
+    "rstring":      (27,r'\$string',"make"),
+    "print":        (28,r'\$print',"label"),
+    "string":       (29,r'\".*\"',"")
 }
 
 sym_table = {"none": (0,"integer")}
@@ -34,15 +42,25 @@ OTHER_LEXEMA = 255
 INSTRUCTION  = 0
 ARGUMENT     = 1
 
-error = 0
+error = False
 idx   = 0
 llen  = 0
 debug_mode = False
 
 output = ""
+spaces = 0
+previous_token = OTHER_LEXEMA
 
 def message(mtype,expected,received,description):
-    print "{}. {} expected, {} received. {}".format(mtype,expected,received,description)
+    global error
+    debug_info = ""
+    if debug_mode:
+        for st in inspect.stack():
+            caller = inspect.getframeinfo(st[0])
+            debug_info = debug_info + "{}:{} At {} ".format(caller.filename,caller.lineno,caller.function)
+    if mtype == "E":
+        error = True
+    print "{}. {} expected, {} received. {} {}".format(mtype,expected,received,description,debug_info)
 
 def get_token(lexema):
     global debug_mode
@@ -57,46 +75,97 @@ def get_token(lexema):
         message("E","TOKEN",lexema,"Does not match any pattern.")
     return ret
 
+def is_operator(token):
+    ret = False
+    if token == tokens["plus"][0] or token == tokens["minus"][0]:
+        ret = True
+    return ret
+
+def is_comparator(token):
+    ret = False
+    if token == tokens[">"][0] or token == tokens["<"][0]:
+        ret = True
+    return ret
+
 def is_definition(token):
     ret = False
     if token == tokens["rint"][0] or token == tokens["rstring"][0]:
         ret = True
     return ret
 
-def is_instruction(token):
+def is_statement(token):
     ret = False
-    if token == tokens["move"][0] or token == tokens["turn"][0] or token == tokens["print"][0]:
+    if token == tokens["move"][0] or token == tokens["turn"][0]:
+        ret = True
+    elif token == tokens["print"][0] or token == tokens["if"][0]:
         ret = True
     return ret
 
 def is_command(token):
     ret = False
-    if token == tokens["arg_right"][0] or tokens["arg_forward"][0] or token == tokens["arg_left"][0]:
+    if token == tokens["arg_right"][0] or token == tokens["arg_left"][0]:
+        ret = True
+    elif token == tokens["arg_forward"][0] or token == tokens["arg_backward"][0]:
         ret = True
     return ret
 
+def is_one_argument(token):
+    ret = False
+    if token == tokens["print"][0] or token == tokens["if"][0]:
+        ret = True
+    return ret
+
+def is_terminal(token):
+    ret = False
+    if token == tokens["number"][0] or token == tokens["identifier"][0]:
+        ret = True
+    elif token == tokens["string"][0]:
+        ret = True
+    return ret
+
+def print_spaces():
+    global spaces
+    global output
+    # write identation
+    id = ''
+    for x in range(spaces):
+        id = id + ' '
+        output = output + id
+
 def generate_code(token,value):
     global output
-    if token == tokens["print"][0]:
-        output = output + "label "
-    elif token == tokens["arg_right"][0]:
-        output = output + "right "
-    elif token == tokens["move"][0] or token == tokens["turn"][0]:
-         previous_token = token
-    elif token == tokens["arg_forward"][0]:
-        output = output + "forward "
-    elif token == tokens["number"][0]:
-        output = output + value + " "
-    elif token == tokens["string"][0]:
-        tvalue = value.replace('\"','')
-        output = output + tvalue + " "
-    elif token == tokens["identifier"][0]:
-        if sym_table.get(value, False):
-            output = output + value + " "
-    elif token == tokens["rint"][0] or token == tokens["rstring"][0]:
-        output = output + "make "
+    global previous_token
+    global error
+
+    if is_statement(token):
+        print_spaces()
+    if not is_terminal(token):
+        found = False
+        keyt = ""
+        for key in tokens:
+            if token == tokens[key][0]:
+                found = True
+                keyt = key
+                break
+        if found:
+            if token == tokens["move"][0] or token == tokens["turn"][0]:
+                previous_token = token
+            if tokens[keyt][2]:
+                output = output + tokens[keyt][2] + ' '
+        else:
+            error = True
+            message("E",token,value,"No rule for this expression gen_code.")
     else:
-        message("E",token,value,"No rule for this expression.")
+        caller = inspect.getframeinfo(inspect.stack()[1][0])
+        valuet = value
+        if "Variables" == caller.function:
+            if token == tokens["string"][0] or token == tokens["identifier"][0]:
+                if token == tokens["string"][0]:
+                    valuet = valuet.replace('\"','')
+                valuet = '\"' + valuet
+        output = output + valuet
+        if "Variables" == caller.function:
+            output = output + ' '
 
 def Variables(sequence):
     global idx
@@ -112,7 +181,6 @@ def Variables(sequence):
          variable = sequence[idx]
          if token == tokens["identifier"][0]:
              sym_table[variable] = (0, OTHER_LEXEMA)
-             output = output + "\""
              generate_code(token,sequence[idx])
              idx = idx + 1
          else:
@@ -135,7 +203,6 @@ def Variables(sequence):
          elif token == tokens["string"][0]:
              if ttype == tokens["rstring"][0]:
                  sym_table[variable] = (sequence[idx], tokens["rstring"][0])
-                 output = output + "\""
                  generate_code(token,sequence[idx])
              else:
                  message("W","string",sequence[idx],"Incorrect assignement.")
@@ -148,17 +215,16 @@ def Variables(sequence):
 def Defines(sequence):
     global idx
     global llen
-    
     token = get_token(sequence[idx])
     while token == tokens["comment"][0] and idx < llen:
         idx = idx + 1
         token = get_token(sequence[idx])
-    
+    # process a definition
     if token == tokens["define"][0] and idx < llen:
         idx = idx + 1
         Variables(sequence)
     else:
-        error = 1
+        error = True
         message("E","$define",sequence[idx])
 
 def Begin(sequence):
@@ -175,11 +241,53 @@ def Argument(sequence):
     global llen
     token = get_token(sequence[idx])
     lexema = sequence[idx]
-    if is_command(token): 
+    if is_command(token):
         generate_code(token,"")
         idx = idx + 1
     else:
         message("E","command",sequence[idx],"")
+
+def ArithmeticExpression(sequence):
+    global idx
+    global llen
+    global output
+
+    token = get_token(sequence[idx])
+    if is_terminal(token):
+        generate_code(token,sequence[idx])
+        idx = idx + 1
+        token = get_token(sequence[idx])
+        if is_operator(token):
+            output = output + sequence[idx]
+            idx = idx + 1
+            ArithmeticExpression(sequence)
+        elif is_comparator(token):
+            output = output + sequence[idx]
+            idx = idx +1
+            LogicalExpression(sequence)
+    else:
+        message("E","Expression",sequence[idx],"")
+
+def LogicalExpression(sequence):
+    global idx
+    global llen
+    global output
+
+    token = get_token(sequence[idx])
+    if is_terminal(token):
+        generate_code(token,sequence[idx])
+        idx = idx + 1
+        token = get_token(sequence[idx])
+        if is_operator(token):
+            output = output + sequence[idx]
+            idx = idx + 1
+            ArithmeticExpression(sequence)
+        elif is_comparator(token):
+            output = output + sequence[idx]
+            idx = idx +1
+            LogicalExpression(sequence)
+    else:
+        message("E","Expression",sequence[idx],"")
 
 def Expression(sequence):
     global idx
@@ -187,30 +295,36 @@ def Expression(sequence):
     global output
 
     token = get_token(sequence[idx])
-    if token == tokens["number"][0] or token == tokens["identifier"][0] or token == tokens["string"][0]:
-        if token == tokens["identifier"][0]:
-            if sym_table.get(sequence[idx],False):
-                if sym_table[sequence[idx]][1] == tokens["rstring"][0]:
-                    output = output + ":"
+    if is_terminal(token):
         generate_code(token,sequence[idx])
         idx = idx + 1
+        token = get_token(sequence[idx])
+        if is_operator(token):
+            output = output + sequence[idx]
+            idx = idx + 1
+            ArithmeticExpression(sequence)
+        elif is_comparator(token):
+            output = output + sequence[idx]
+            idx = idx +1
+            LogicalExpression(sequence)
     else:
-        message("E","Number",sequence[idx],"")
+        message("E","Expression",sequence[idx],"")
 
-def Commands(sequence):
+def Statements(sequence):
     global idx
     global llen
     global output
+    global spaces
 
     t1 = OTHER_LEXEMA
     token = get_token(sequence[idx])
-    while is_instruction(token):
+    while is_statement(token):
         arguments = 2
         t1 = token
-        # generate coode according for the instruction
+        # generate code according for the instruction
         generate_code(token,"")
         # check if it is one argument command
-        if token == tokens["print"][0]:
+        if is_one_argument(token):
             arguments = 1
         # increment index to check next token
         idx = idx + 1
@@ -236,15 +350,30 @@ def Commands(sequence):
                 message("E",")",sequence[idx],"")
         else:
             message("E","(",sequence[idx],"")
-        output = output + '\n'
+        # if this is the body of an if sentence
+        if t1 == tokens["if"][0]:
+            Begin(sequence)
+            if False == error:
+                print_spaces()
+                output = output + '[\n'
+                spaces = spaces + 1
+            Statements(sequence)
+            End(sequence)
+            if False == error:
+                spaces = spaces - 1
+                print_spaces()
+                output = output + ']'
         # get a new token to continue processing more commands
         token = get_token(sequence[idx])
+        output = output + '\n'
 
 def End(sequence):
     global idx
     global llen
     token = get_token(sequence[idx])
-    if token != tokens["end"][0]:
+    if token == tokens["end"][0]:
+        idx = idx + 1
+    else:
         message("E","$end",sequence[idx],"")
 
 def Compile(sequence):
@@ -254,7 +383,7 @@ def Compile(sequence):
     idx = 0
     Defines(sequence)
     Begin(sequence)
-    Commands(sequence)
+    Statements(sequence)
     End(sequence)
 
 def main():
@@ -264,7 +393,7 @@ def main():
     contents = []
     with open(sys.argv[1], 'r') as f:
         contents = f.read()
-        contents = re.split(r'(\".*\"|\n|[ ]|//.*|\d+|\$[a-zA-Z]+|[a-zA-Z][a-zA-Z0-9_]*|\(|\))',contents)
+        contents = re.split(r'(\".*\"|\n|[ ]|//.*|\d+|\$[a-zA-Z]+|[a-zA-Z][a-zA-Z0-9_]*|\(|\)|<|>|\/|\*)',contents)
     
     while '' in contents: contents.remove('')
     while ' ' in contents: contents.remove(' ')
